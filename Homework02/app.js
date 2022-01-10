622b310e7d87e153340db6769a8b119e8c5805af
niloofaryaghubi69 <87565460+niloofaryaghubi69@users.noreply.github.com>
2022-01-10 21:50:12 +0330

    Add files via upload

---
+++ Homework02/app.js
@@ -0,0 +1,24 @@
+var a = 5;
+var b = 10;
+var c = "5";
+var d = false;
+
+
+
+
+
+
+
+console.log((!(b<5) && b >= (a*2)))
+
+console.log((!(a == c) || a !== c))
+
+console.log((d == false && b-7 > a && d))
+
+console.log((a && b && c))
+
+console.log((!a || !d == true || !d))
+
+console.log((a >= (b-5) && 0))
+
+console.log((arr || false))
